6eae458d21ca7e8fcf4f9fcc60be992987d3ca09
Grant Powell <email@grantjamespowell.com>
2021-01-21 17:22:23 -0500

    Add in vim projections

--- lib/botskrieg_arena.ex
+++ lib/botskrieg_arena.ex
@@ -1,17 +1,4 @@
 defmodule BotskriegArena do
-  @moduledoc """
-  Documentation for `BotskriegArena`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> BotskriegArena.hello()
-      :world
-
-  """
   def hello do
     :world
   end
