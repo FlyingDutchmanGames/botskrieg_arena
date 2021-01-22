fc82e45a4e7b0915b09943d24a1054f264660be4
Grant Powell <email@grantjamespowell.com>
2021-01-21 19:16:31 -0500

    Test sandboxing

--- test/botskrieg_arena/sandbox/lua_test.exs
+++ test/botskrieg_arena/sandbox/lua_test.exs
@@ -7,6 +7,18 @@ defmodule BotskriegArena.Sandbox.LuaTest do
     assert %Lua.State{} = Lua.init()
   end
 
+  describe "run" do
+    test "you can run some lua code in the sandbox" do
+      state = Lua.init()
+      assert {[2.0], _} = Lua.run(state, "return 1 + 1")
+    end
+
+    test "It can't run sandboxed things" do
+      state = Lua.init()
+      assert {:error, _} = Lua.run(state, "return os.getenv(\"HOME\")")
+    end
+  end
+
   describe "set_on_table/read_from_table" do
     test "you can set and read from a table from elixir" do
       state = Lua.init()
@@ -23,7 +35,10 @@ defmodule BotskriegArena.Sandbox.LuaTest do
 
       state = Lua.set_on_table(state, ["a"], %{"b" => "c", "d" => "e"})
       assert {[{"b", "c"}, {"d", "e"}], state} = Lua.read_from_table(state, ["a"])
-      assert {"c", _state} = Lua.read_from_table(state, ["a", "b"])
+      assert {"c", state} = Lua.read_from_table(state, ["a", "b"])
+
+      {[], state} = Lua.run(state, "hello_table = { hello=\"world\" };")
+      assert {"world", _state} = Lua.read_from_table(state, ["hello_table", "hello"])
     end
   end
 end
